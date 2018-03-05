2fc020c893e929dff0a2ab45952dce764f3a1922
CreamYang <ctycream@gmail.com>
2018-03-05 21:51:50 +0800

    fix

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 var turn = 0;
 var f = 0;
 $(document).ready(function () {
-  $(".sb").mouseenter(function(){
-    $(this).css("background","#555");
-    $(this).css("color","#FFF");
-  });
-  $(".sb").mouseleave(function(){
-    $(this).css("background","#DDD");
-    $(this).css("color","#222");
-  });
   $(".sb").click(function() {
     if (f != 1) {
       if (turn == 0 && $(this).text() == "") {
@@ -21,14 +13,6 @@ $(document).ready(function () {
       }
       judgement();
     }
-  });
-  $(".newbutton").mouseenter(function(){
-    $(this).css("background","#555");
-    $(this).css("color","#FFF");
-  });
-  $(".newbutton").mouseleave(function(){
-    $(this).css("background","#DDD");
-    $(this).css("color","#222");
   });
   $('.newbutton').click(function() {
     turn=0;
